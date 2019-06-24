b3afd0d8323dd037a0cd13d0ea0da1f4808467f6
InveterateCoder <inveterate.coder@gmail.com>
2019-06-24 20:22:21 +0300

    single authorization page

--- AFCitizen/Pages/Authorize.cshtml.cs
+++ AFCitizen/Pages/Authorize.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,12 +11,93 @@ namespace AFCitizen.Pages
 {
     public class AuthorizeModel : PageModel
     {
-        [BindProperty]
+        [Required, BindProperty]
         public string Email { get; set; }
-        [BindProperty]
+        [Required, BindProperty]
         public string Password { get; set; }
-        public void OnGet()
+        public async Task<IActionResult> OnPostAsync([FromServices]UserManager<IdentityUser> userMgr, [FromServices]SignInManager<IdentityUser> signinMgr)
         {
+            await signinMgr.SignOutAsync();
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userMgr.FindByEmailAsync(Email);
+                if (user != null)
+                {
+                    if(await userMgr.IsInRoleAsync(user, "Город"))
+                    {
+                        var result = await signinMgr.PasswordSignInAsync(user, Password, false, false);
+                        if (result.Succeeded)
+                        {
+                            if (await userMgr.IsInRoleAsync(user, "Диспетчер"))
+                            {
+                                return RedirectToPage("/City/Index");
+                            }
+                            else
+                            {
+                                return RedirectToPage("/City/Agent");
+                            }
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Не получилось авторизоваться");
+                        }
+                    }
+                    else
+                    {
+                        if (await userMgr.IsInRoleAsync(user, "Субъект"))
+                        {
+                            var result = await signinMgr.PasswordSignInAsync(user, Password, false, false);
+                            if (result.Succeeded)
+                            {
+                                if (await userMgr.IsInRoleAsync(user, "Диспетчер"))
+                                {
+                                    return RedirectToPage("/Subject/Index");
+                                }
+                                else
+                                {
+                                    return RedirectToPage("/Subject/Agent");
+                                }
+                            }
+                            else
+                            {
+                                ModelState.AddModelError("", "Не получилось авторизоваться");
+                            }
+                        }
+                        else
+                        {
+                            if (await userMgr.IsInRoleAsync(user, "Федерация"))
+                            {
+                                var result = await signinMgr.PasswordSignInAsync(user, Password, false, false);
+                                if (result.Succeeded)
+                                {
+                                    if (await userMgr.IsInRoleAsync(user, "Диспетчер"))
+                                    {
+                                        return RedirectToPage("/Federation/Index");
+                                    }
+                                    else
+                                    {
+                                        return RedirectToPage("/Federation/Agent");
+                                    }
+                                }
+                                else
+                                {
+                                    ModelState.AddModelError("", "Не получилось авторизоваться");
+                                }
+                            }
+                            else
+                                ModelState.AddModelError("", "Исполнитель не найден");
+                        }
+                    }
+                }
+                else
+                    ModelState.AddModelError("", "Исполнитель не найден");
+            }
+            return Page();
+        }
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
         }
     }
 }
--- AFCitizen/Pages/Federation/Agent.cshtml.cs
+++ AFCitizen/Pages/Federation/Agent.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AFCitizen.Pages.Region
+namespace AFCitizen.Pages.Federation
 {
-    public class IndexModel : PageModel
+    public class AgentModel : PageModel
     {
         public void OnGet()
         {
