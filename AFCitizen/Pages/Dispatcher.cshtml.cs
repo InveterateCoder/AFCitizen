6ffe61673bddd9ec13a26300f22004836b1516a1
InveterateCoder <inveterate.coder@gmail.com>
2019-06-28 21:01:53 +0300

    teached dispatcher to retrieve data from db

--- AFCitizen/Pages/Dispatcher.cshtml.cs
+++ AFCitizen/Pages/Dispatcher.cshtml.cs
@@ -11,10 +11,23 @@ namespace AFCitizen.Pages
     [Authorize(Policy = "ƒиспетчер")]
     public class DispatcherModel : PageModel
     {
-        public string Header { get; set; }
-        public void OnGet()
+        public string Authority { get; set; }
+        public IEnumerable<Models.Block> OpenBlocks;
+        public async Task<IActionResult> OnGetAsync()
         {
-            Header = User.Identity.Name;
+            Authority = User.Identity.Name;
+            Models.ILevelDbContext levelDbContext = null;
+            if (User.IsInRole("ѕерв”ровень"))
+                levelDbContext = HttpContext.RequestServices.GetService(typeof(Models.FirstLevelDbContext)) as Models.ILevelDbContext;
+            else if (User.IsInRole("—ред”ровень"))
+                levelDbContext = HttpContext.RequestServices.GetService(typeof(Models.MidLevelDbContext)) as Models.ILevelDbContext;
+            else if (User.IsInRole("“оп”ровень"))
+                levelDbContext = HttpContext.RequestServices.GetService(typeof(Models.TopLevelDbContext)) as Models.ILevelDbContext;
+            if (levelDbContext == null)
+                return RedirectToPage("Error");
+            OpenBlocks = await Task.Run(() => levelDbContext.Blocks.Where(block => block.To == Authority && block.Type == Models.BlockType.Open
+                  && !levelDbContext.Blocks.Any(rep => (rep.DocId == block.DocId && block.Type != Models.BlockType.Open))).ToArray());
+            return Page();
         }
     }
 }
