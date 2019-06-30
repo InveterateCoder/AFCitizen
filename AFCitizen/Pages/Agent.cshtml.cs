ffeb2312581694277299f090a46a5e0db31139c3
InveterateCoder <inveterate.coder@gmail.com>
2019-06-30 09:54:50 +0300

    changed

--- AFCitizen/Pages/Agent.cshtml.cs
+++ AFCitizen/Pages/Agent.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AFCitizen.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,9 +13,21 @@ namespace AFCitizen.Pages
     public class AgentModel : PageModel
     {
         public string Header { get; set; }
-        public void OnGet()
+        public IEnumerable<Block> OpenBlocks { get; set; }
+        public IActionResult OnGet()
         {
             Header = User.Identity.Name;
+            ILevelDbContext levelDbContext = null;
+            if (User.IsInRole("ѕерв”ровень"))
+                levelDbContext = HttpContext.RequestServices.GetService(typeof(FirstLevelDbContext)) as ILevelDbContext;
+            else if (User.IsInRole("—ред”ровень"))
+                levelDbContext = HttpContext.RequestServices.GetService(typeof(MidLevelDbContext)) as ILevelDbContext;
+            else if (User.IsInRole("“оп”ровень"))
+                levelDbContext = HttpContext.RequestServices.GetService(typeof(TopLevelDbContext)) as ILevelDbContext;
+            if (levelDbContext == null)
+                return RedirectToPage("Error");
+            OpenBlocks = levelDbContext.Blocks.Where(block => block.To == User.Identity.Name && block.Type == BlockType.Accept && !levelDbContext.Blocks.Any(ab => ab.DocId == block.DocId && ab.Type == BlockType.Close));
+            return Page();
         }
     }
 }
